1c182365a8f42d9e55500a838944dd08c2005b06
leejimqiu <leejimqiu@gmail.com>
2021-12-07 21:10:44 +0800

    fix: update command order

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import path from 'path';
+
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import fs from 'fs-extra';
-import ci from './lib/cli';
+
 import config from './config';
+import ci from './lib/cli';
 
 async function run(): Promise<void> {
   try {
@@ -18,34 +20,40 @@ async function run(): Promise<void> {
 
     const { MINI_APP_ID, MINI_APP_PRIVATE_KEY, GITHUB_WORKSPACE: sourceDir = '' } = process.env;
     const uploadDir = path.join(sourceDir, projectPath);
-    
+
     const timestamp = new Date().getTime();
     const privateKeyDir = `./private.${timestamp}.key`;
     await fs.outputFile(privateKeyDir, MINI_APP_PRIVATE_KEY);
 
-    const commandOptions = options.replace('\n', '').split(' ').map(v => {
-      const map = v.split('=');
-      if(map[1]) {
-        return `${map[0]} ${map[1]}`;
-      }
-
-      return `${map[0]} true`;
-    });
-
-    const existsRobotConfig = await fs.pathExists(path.join(sourceDir, '.mini-program-robot.js'))
-    
-    let robotConfig: any= {};
-
-    if(robotsAttr) {
-      robotsAttr.replace('\n', '').split(' ').forEach(v => {
+    const commandOptions = options
+      .replace('\n', '')
+      .split(' ')
+      .map(v => {
         const map = v.split('=');
-        if(map[1]) {
-          robotConfig[map[0]] = map[1];
-        } else {
-          robotConfig[map[0]] = 28;
+        if (map[1]) {
+          return `${map[0]} ${map[1]}`;
         }
+
+        return `${map[0]} true`;
       });
-    } else if(existsRobotConfig) {
+
+    const existsRobotConfig = await fs.pathExists(path.join(sourceDir, '.mini-program-robot.js'));
+
+    let robotConfig: any = {};
+
+    if (robotsAttr) {
+      robotsAttr
+        .replace('\n', '')
+        .split(' ')
+        .forEach(v => {
+          const map = v.split('=');
+          if (map[1]) {
+            robotConfig[map[0]] = map[1];
+          } else {
+            robotConfig[map[0]] = 28;
+          }
+        });
+    } else if (existsRobotConfig) {
       robotConfig = require(path.join(sourceDir, '.mini-program-robot.js'));
     } else {
       robotConfig = config;
@@ -55,7 +63,9 @@ async function run(): Promise<void> {
     const branch = github.context.ref.replace(/refs\/heads\//, '');
     const pullRuestTitle = github.context.payload.pull_request?.title;
     const robot = robotConfig[branch] || robotConfig[author] || 28;
-    const commits = github.context.payload.commits || [{message: `robot ${robot} trigger this pub`}];
+    const commits = github.context.payload.commits || [
+      { message: `robot ${robot} trigger this pub` },
+    ];
 
     const project = new ci({
       sourceDir,
@@ -67,18 +77,32 @@ async function run(): Promise<void> {
         'miniprogram-ci',
         ...subcommand.split(' '),
         `${actionType}`,
-        '--project-type', `${projectType}`,
-        '--pp', `${uploadDir}`,
-        '--pkp', `${privateKeyDir}`,
-        '--appid', `${MINI_APP_ID}`,
-        '--uv', `${version}`,
-        '--ud', `'${remark || pullRuestTitle || commits[0].message}'`,
-        '-r', `${robot}`,
         ...commandOptions,
-      ]
-    })
+        '--project-type',
+        `${projectType}`,
+        '--pp',
+        `${uploadDir}`,
+        '--pkp',
+        `${privateKeyDir}`,
+        '--appid',
+        `${MINI_APP_ID}`,
+        '--uv',
+        `${version}`,
+        '--ud',
+        `'${remark || pullRuestTitle || commits[0].message}'`,
+        '-r',
+        `${robot}`,
+      ],
+    });
 
-    const handle: 'cloud' | 'get_dev_source_map' | 'pack_npm' | 'preview' | 'upload' | 'pack_npm_manually' | 'custom' = actionType.replace(/\-/, '_') as any;
+    const handle:
+      | 'cloud'
+      | 'get_dev_source_map'
+      | 'pack_npm'
+      | 'preview'
+      | 'upload'
+      | 'pack_npm_manually'
+      | 'custom' = actionType.replace(/-/, '_') as any;
 
     await project[handle]();
 
